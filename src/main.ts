e2ca67bb943ddad60b36ad4b71dcf6a030e644fb
daemontplx <165395025+daemontplx@users.noreply.github.com>
2025-06-03 10:37:59 +0800

    chore: updated url for swagger docs

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Tensorplex Kami')
     .setDescription(
-      `Ultra-light server library written in TypeScript for interacting with the Bittensor chain.\n\nExample code samples provided work with the Kami boilerplate Python code in the repo [here](https://github.com/tensorplex-labs/kami/tree/main/docs/python-examples).\n\nYou may also test out the endpoints through [Swagger UI](http://localhost:${process.env.KAMI_PORT}/chain/docs) once the Kami instance is running locally.`,
+      `Ultra-light server library written in TypeScript for interacting with the Bittensor chain.\n\nExample code samples provided work with the Kami boilerplate Python code in the repo [here](https://github.com/tensorplex-labs/kami/tree/main/docs/python-examples).\n\nYou may also test out the endpoints through [Swagger UI](http://localhost:${process.env.KAMI_PORT}/docs) once the Kami instance is running locally.`,
     )
     .setVersion('1.0')
     .addServer(`http://localhost:${process.env.KAMI_PORT}`, 'Development')
