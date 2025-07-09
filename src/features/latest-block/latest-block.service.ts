0050aa03f56e2df73867ed1ceae00104b090d86a
daemontplx <165395025+daemontplx@users.noreply.github.com>
2025-07-09 16:09:22 +0800

    fix: force kami to exit upon substrate error, removed try catch on service since its handled by exception filters already

--- src/features/latest-block/latest-block.service.ts
+++ src/features/latest-block/latest-block.service.ts
@@ -1,9 +1,7 @@
-import { SubtensorException } from 'src/core/substrate/exceptions/substrate-client.exception';
 import { SubstrateConnectionService } from 'src/core/substrate/services/substrate-connection.service';
 
-import { HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
-import { LatestBlockException } from './latest-block.exception';
 import { BlockInfo } from './latest-block.interface';
 
 @Injectable()
@@ -13,29 +11,17 @@ export class LatestBlockService {
   constructor(private readonly substrateConnectionService: SubstrateConnectionService) {}
 
   async getLatestBlock(): Promise<BlockInfo> {
-    try {
-      const client = await this.substrateConnectionService.getClient();
+    const client = await this.substrateConnectionService.getClient();
 
-      this.logger.debug(`Retrieving latest block`);
-      const blockHead = await client.rpc.chain.getHeader();
-      const result: BlockInfo = {
-        blockNumber: blockHead.number.toNumber(),
-        parentHash: blockHead.parentHash.toHex(),
-        stateRoot: blockHead.stateRoot.toHex(),
-        extrinsicsRoot: blockHead.extrinsicsRoot.toHex(),
-      };
-      this.logger.debug(`Latest block: ${JSON.stringify(result)}`);
-      return result;
-    } catch (error) {
-      if (error instanceof SubtensorException) {
-        throw error;
-      }
-      throw new LatestBlockException(
-        HttpStatus.INTERNAL_SERVER_ERROR,
-        'UNKNOWN',
-        error.message,
-        error.stack,
-      );
-    }
+    this.logger.debug(`Retrieving latest block`);
+    const blockHead = await client.rpc.chain.getHeader();
+    const result: BlockInfo = {
+      blockNumber: blockHead.number.toNumber(),
+      parentHash: blockHead.parentHash.toHex(),
+      stateRoot: blockHead.stateRoot.toHex(),
+      extrinsicsRoot: blockHead.extrinsicsRoot.toHex(),
+    };
+    this.logger.debug(`Latest block: ${JSON.stringify(result)}`);
+    return result;
   }
 }
--- src/features/serve-axon/serve-axon.service.ts
+++ src/features/serve-axon/serve-axon.service.ts
@@ -1,11 +1,9 @@
-import { SubtensorException } from 'src/core/substrate/exceptions/substrate-client.exception';
 import { SubstrateClientService } from 'src/core/substrate/services/substrate-client.service';
 import { SubstrateConnectionService } from 'src/core/substrate/services/substrate-connection.service';
 
-import { HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 import { AxonCallParams } from './serve-axon.call-params.interface';
-import { ServeAxonException } from './serve-axon.exception';
 
 @Injectable()
 export class ServeAxonService {
@@ -17,35 +15,21 @@ export class ServeAxonService {
   ) {}
 
   async serveAxon(params: AxonCallParams): Promise<string> {
-    try {
-      const client = await this.substrateConnectionService.getClient();
+    const client = await this.substrateConnectionService.getClient();
 
-      // Create the transaction using the client
-      const axonTx = client.tx.subtensorModule.serveAxon(
-        params.netuid,
-        params.version,
-        params.ip,
-        params.port,
-        params.ipType,
-        params.protocol,
-        params.placeholder1,
-        params.placeholder2,
-      );
+    // Create the transaction using the client
+    const axonTx = client.tx.subtensorModule.serveAxon(
+      params.netuid,
+      params.version,
+      params.ip,
+      params.port,
+      params.ipType,
+      params.protocol,
+      params.placeholder1,
+      params.placeholder2,
+    );
 
-      // Use the client service to sign and send the transaction
-      return await this.substrateClientService.signAndSendTransaction(axonTx);
-    } catch (error) {
-      this.logger.error(`Error serving axon: ${error.message}`);
-      if (error instanceof SubtensorException) {
-        throw error;
-      }
-
-      throw new ServeAxonException(
-        HttpStatus.INTERNAL_SERVER_ERROR,
-        'UNKNOWN',
-        error.message,
-        error.stack,
-      );
-    }
+    // Use the client service to sign and send the transaction
+    return await this.substrateClientService.signAndSendTransaction(axonTx);
   }
 }
